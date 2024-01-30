d0ca10269112357cd7cce4b4ba68f5868d1a214d
HighBlacK <hcourtilleau@gmail.com>
2024-01-30 20:21:04 +0100

    Custom folder import

---
+++ lua/custom/plugins.lua
@@ -0,0 +1,50 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust_analyzer",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = '^4',
+    ft = {'rust'},
+  },
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function ()
+      vim.g.rustfmt_autosave = 1
+    end
+  },
+  {
+    "saecki/crates.nvim",
+    ft = {"rust", "toml"},
+    config = function (_, opts)
+      local crate = require('crates')
+      crates.setup(opts)
+      crates.show()
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function ()
+      local M = require "plugins.configs.cmp"
+      table.insert(M.sources, {name = "crates"})
+      return M
+    end,
+  },
+  {
+    "github/copilot.vim",
+  },
+}
+return plugins
